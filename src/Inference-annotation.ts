c73d470b1aa58c7502552979c04ff46545694bd3
Jinal <jpatel111102@gmail.com>
2026-02-12 13:53:36 +0530

    Inference and annotation

---
+++ src/Inference-annotation.ts
@@ -0,0 +1,14 @@
+//+++++++++++++++++++++++++ Inference ++++++++++++++++++++++++++=
+
+// jate j type samji le chhe ts ma define karvani jarur nathi input na data thi j type guess kari le chhe
+
+let name = "Jinal";
+
+
+//+++++++++++++++++++++++ Annotation +++++++++++++++++++++++==
+
+// type define karvu pade chhe ke data kaya type na chhe
+
+function add(a: number, b: number): number {
+  return a + b;
+}
